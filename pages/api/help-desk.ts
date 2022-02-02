9687b46955657ea9ca0656975149e664e7a19602
Diego Buriti <bs.diego97@gmail.com>
2022-02-02 15:19:01 -0300

    feat: add server config and connection with database

---
+++ pages/api/help-desk.ts
@@ -0,0 +1,36 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { NowRequest, NowResponse } from '@vercel/node'
+import { MongoClient, Db } from 'mongodb'
+import { parse } from 'url'
+
+let cachedDb: Db | null = null
+
+async function connectToDatabase(uri: string) {
+  if (!!cachedDb) return cachedDb
+
+  const client = await MongoClient.connect(uri)
+  const dbName = parse(uri).pathname?.substring(1)
+  const dataBase = client.db(dbName)
+
+  cachedDb = dataBase
+
+  return dataBase
+}
+
+export default async function handler(
+  request: NowRequest,
+  response: NowResponse
+) {
+  const content = request.body
+  const db = await connectToDatabase(process.env.MONGODB_URI!!)
+
+  const collection = db.collection('helpdesk')
+
+  console.log('contebnt', content)
+
+  await collection.insertOne({
+    name: content.name
+  })
+
+  return response.status(201).json({ ok: true })
+}
