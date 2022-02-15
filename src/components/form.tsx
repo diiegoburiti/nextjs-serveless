c16a46f6af07219cbd0adf2444e18f4cf4293227
Diego Buriti <bs.diego97@gmail.com>
2022-02-15 08:59:10 -0300

    refactor: refactor form component

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -9,53 +9,46 @@ import {
   Select,
   Button
 } from '@chakra-ui/react'
-import { FormEvent, useState } from 'react'
-import { sendRequest } from '../service/request'
+import { ChangeEvent, FormEvent, useState } from 'react'
+import { sendRequest } from '~/service/request'
 import styles from './styles.module.css'
 
 type ValuesType = {
-  subjectText: string
-  descriptionText?: string
+  subject: string
+  description?: string
   ticketType?: string
 }
 
-type FormComponentProps = {
-  onSubmitForm: ({
-    subjectText,
-    descriptionText,
-    ticketType
-  }: ValuesType) => void
-}
+export const FormComponent = () => {
+  const ticketsType = ['Internet', 'Hardware', 'Software']
+  const [formValues, setFormValues] = useState<ValuesType>({} as ValuesType)
 
-export const FormComponent = ({ onSubmitForm }: FormComponentProps) => {
-  const [subjectText, setSubjectText] = useState('')
-  const [descriptionText, setDescriptionText] = useState<string>()
-  const [ticketType, setTicketType] = useState<string>()
-
-  const [formValue, serFormValue] = useState<ValuesType>({
-    ticketType: '',
-    descriptionText: '',
-    subjectText: ''
-  })
-  const handleSubmit = (event: FormEvent) => {
-    event.preventDefault()
-    sendRequest('/api/help-desk', {
-      subjectText,
-      descriptionText,
-      ticketType
+  const handleFormValues = (
+    event: ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
+    setFormValues((prevState) => {
+      return {
+        ...prevState,
+        [event.target.name]: event.target.value
+      }
     })
   }
 
-  const ticketsType = ['Internet', 'Hardware', 'Software']
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault()
+    sendRequest('/api/help-desk', {
+      ...formValues
+    })
+  }
 
   return (
-    <Container maxW="container.lg" m="0 auto">
+    <Container maxW="container.lg" m="2rem auto ">
       <Flex justifyContent="center" flexDir="column">
         <Heading color="fontColor" as="h1" size="3xl" textAlign="center">
           Help Desk
         </Heading>
 
-        <Text color="fontColor" as="h5" size="sm" textAlign="center">
+        <Text color="fontColor" as="h5" size="sm" textAlign="center" mt="2rem">
           Register your problem to our help desk team
         </Text>
 
@@ -67,10 +60,11 @@ export const FormComponent = ({ onSubmitForm }: FormComponentProps) => {
               </FormLabel>
               <Input
                 id="subject"
-                type="subject"
+                type="text"
                 color="fontColor"
+                name="subject"
                 placeholder="Type the subject"
-                onChange={(event) => setSubjectText(event.target.value)}
+                onChange={handleFormValues}
               />
             </FormControl>
             <FormControl>
@@ -79,10 +73,11 @@ export const FormComponent = ({ onSubmitForm }: FormComponentProps) => {
               </FormLabel>
               <Input
                 id="description"
-                type="description"
+                type="text"
+                name="description"
                 color="fontColor"
                 placeholder="Type a description if necessary"
-                onChange={(event) => setDescriptionText(event.target.value)}
+                onChange={handleFormValues}
               />
             </FormControl>
 
@@ -91,7 +86,8 @@ export const FormComponent = ({ onSubmitForm }: FormComponentProps) => {
                 placeholder="Select ticket type"
                 color="fontColor"
                 w="20vw"
-                onChange={(event) => setTicketType(event.target.value)}
+                name="ticketType"
+                onChange={handleFormValues}
               >
                 {ticketsType.map((item) => (
                   <option key={item} value={item}>
@@ -100,7 +96,7 @@ export const FormComponent = ({ onSubmitForm }: FormComponentProps) => {
                 ))}
               </Select>
 
-              <Button type="submit" disabled={!subjectText}>
+              <Button type="submit" disabled={!formValues.subject}>
                 Submit
               </Button>
             </Flex>
